ccb1aa355d3afdd2483e6dce43e93711e4e2bdd4
Sophie Elliott <82548883+19179@users.noreply.github.com>
2022-09-01 23:44:50 +0000

    added some comments to the code to help the layout of how the code gonna be

---
+++ main.py
@@ -0,0 +1,30 @@
+# import statement
+
+#funtions go here 
+
+
+
+# Ask for users name
+
+
+# ********** Main Routine **********
+
+# setting up budget
+
+# How much items there are
+
+# Get the items that they want to sort
+
+# Get price for each item 
+
+# Ask user for info on items
+
+# Convert it into kgs
+
+# show the items in order of good prices 
+
+# suggest the product that is good for the budget
+
+# ask some questions from user about the products 
+
+# suggest the product to what the user feels like
